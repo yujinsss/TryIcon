3c78be0733205ca9a4aa8544811b06b2540cda03
Yujin Lee <lyj62346972@gmail.com>
2019-06-18 14:49:28 +0900

    #2 트레이아이콘 폼 디자인 by 3214 이유진

--- TryIcon/Form1.Designer.cs
+++ TryIcon/Form1.Designer.cs
@@ -28,39 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnTray = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.cmsMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ntiTray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.폼보이기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnTray
             // 
-            this.btnTray.Location = new System.Drawing.Point(12, 12);
+            this.btnTray.Location = new System.Drawing.Point(10, 10);
+            this.btnTray.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnTray.Name = "btnTray";
-            this.btnTray.Size = new System.Drawing.Size(385, 100);
+            this.btnTray.Size = new System.Drawing.Size(337, 80);
             this.btnTray.TabIndex = 0;
             this.btnTray.Text = "트레이로 이동";
             this.btnTray.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(12, 125);
+            this.label1.Location = new System.Drawing.Point(10, 100);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(385, 26);
+            this.label1.Size = new System.Drawing.Size(337, 21);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Developed by 3199 ㅇㅇㅇ";
+            this.label1.Text = "Developed by 3214 이유진";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // cmsMenu
+            // 
+            this.cmsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.폼보이기ToolStripMenuItem,
+            this.종료ToolStripMenuItem});
+            this.cmsMenu.Name = "cmsMenu";
+            this.cmsMenu.Size = new System.Drawing.Size(181, 70);
+            // 
+            // ntiTray
+            // 
+            this.ntiTray.ContextMenuStrip = this.cmsMenu;
+            this.ntiTray.Icon = ((System.Drawing.Icon)(resources.GetObject("ntiTray.Icon")));
+            this.ntiTray.Text = "트레이 아이콘";
+            // 
+            // 폼보이기ToolStripMenuItem
+            // 
+            this.폼보이기ToolStripMenuItem.Name = "폼보이기ToolStripMenuItem";
+            this.폼보이기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.폼보이기ToolStripMenuItem.Text = "폼 보이기";
+            // 
+            // 종료ToolStripMenuItem
+            // 
+            this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.종료ToolStripMenuItem.Text = "종료";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(409, 160);
+            this.ClientSize = new System.Drawing.Size(358, 128);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnTray);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "트레이 아이콘 by 3199 ㅇㅇㅇ";
+            this.Text = "트레이 아이콘 by 3214 이유진";
+            this.cmsMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -69,6 +106,10 @@
 
         private System.Windows.Forms.Button btnTray;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip cmsMenu;
+        private System.Windows.Forms.NotifyIcon ntiTray;
+        private System.Windows.Forms.ToolStripMenuItem 폼보이기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
     }
 }
 
